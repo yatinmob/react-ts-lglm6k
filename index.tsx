ec65fedad23fd48a990540629b00a315fb59d474
yatinmob <ypatel@mobiquityinc.com>
2021-12-09 13:40:35 +0530

    2 files modified, 3 added, 2 deleted

--- index.tsx
+++ index.tsx
@@ -1,28 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+import App from './App';
 import './style.css';
-
-interface AppProps {}
-interface AppState {
-  name: string;
-}
-
-class App extends Component<AppProps, AppState> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'There',
-    };
-  }
-
-  render() {
-    return (
-      <div>
-        <Hello />
-      </div>
-    );
-  }
-}
 
 render(<App />, document.getElementById('root'));
