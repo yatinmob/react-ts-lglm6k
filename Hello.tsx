f8fbe2d5a51f54304face52a2ee0a379c85b5af9
yatinmob <ypatel@mobiquityinc.com>
2021-12-09 10:55:03 +0530

    2 files modified, 1 added

--- Hello.tsx
+++ Hello.tsx
@@ -1,3 +1,24 @@
-import React from 'react';
+import * as React from 'react';
+import { ethers } from 'ethers';
+import './Hello.css';
 
-export default ({ name }) => <h1>Hello {name}!</h1>;
+export default function App() {
+  const wave = () => {};
+
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">👋 Hey there!</div>
+
+        <div className="bio">
+          I am farza and I worked on self-driving cars so that's pretty cool
+          right? Connect your Ethereum wallet and wave at me!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
